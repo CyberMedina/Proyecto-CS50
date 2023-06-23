e35e6cdbeba1544a1160bb6d09631b09ac70cdc3
MedinaCat <medinajhonatan02@gmail.com>
2023-06-22 22:40:43 -0600

    base de datos en línea

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ import mysql.connector
 
 def connectionBD():
     db = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="1233456",
+        host="proyectocs50.mysql.database.azure.com",
+        user="localhost",
+        password="cjs-1234",
         database="proyectocs50"
     )
     return db
