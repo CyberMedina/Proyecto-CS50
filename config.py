7a7158fb5cc0ed39343cd3b3b6000cfb85e868d4
MedinaCat <medinajhonatan02@gmail.com>
2023-06-23 17:35:22 -0600

    Intercalación BD config,py

--- config.py
+++ config.py
@@ -5,11 +5,20 @@ import mysql.connector
 
 def connectionBD():
     db = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="1233456",
+        host="proyectocs50.mysql.database.azure.com",
+        user="localhost",
+        password="cjs-1234",
         database="proyectocs50"
     )
     return db
+
+# def connectionBD():
+#     db = mysql.connector.connect(
+#         host="localhost",
+#         user="root",
+#         password="1233456",
+#         database="proyectocs50"
+#     )
+#     return db
 
 
